5cf8c36066a95ac3022e413c2cedef86edf97d8b
Karmhack <78755695+Karmhack@users.noreply.github.com>
2023-05-27 13:03:00 +0200

    Create send_hex.py

---
+++ send_hex.py
@@ -0,0 +1,29 @@
+import socket
+
+def send_hexadecimal(ip, port, hexadecimal):
+    # Create a TCP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect to the target server
+        client_socket.connect((ip, port))
+
+        # Convert the hexadecimal string to bytes
+        hex_bytes = bytes.fromhex(hexadecimal)
+
+        # Send the hexadecimal bytes
+        client_socket.sendall(hex_bytes)
+        print("Hexadecimal data sent successfully.")
+        client_socket.settimeout(5)
+        a=client_socket.recv(100)
+        print(a)
+
+    except ConnectionRefusedError:
+        print("Connection refused. Make sure the server is running and the provided IP and port are correct.")
+
+# Set target's IP, Port and data you want to send
+ip_address = "10.10.10.10"  # Replace with the IP address of the server
+port_number = 4444  # Replace with the port number of the server
+hex_data = "00 00 00 47 c2 45 08 01 01 0a 08 43 42 20 20 20 20 20 20 10 02 00 0a 13 02 00 01 14 01 14 15 02 05 02 17 02 00 05 19 0c 42 4e 50 56 41 55 44 37 39 33 30 32 1a 0c 42 4e 50 56 41 55 44 30 30 30 30 31 1b 01 01 1d 01 01 1e 01 05"  # Replace with the hexadecimal value you want to send
+
+     
